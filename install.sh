57a24af4679b00ebc5d56a6330bb6227ba9e3371
Jeppe Wolff Johansen <jepper123411@proton.me>
2026-03-17 21:25:15 +0100

    Added install script

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+ARCH=$(uname -m)
+VERSION="1.0.0"
+
+case "$ARCH" in
+	x86_64) ARCH="amd64" ;;
+	aarch64|arm64) ARCH="arm64" ;;
+	*) echo "Unsuported arch: $ARCH"; exit 1 ;;
+esac
+
+TMP_DIR=$(mktemp -d)
+cd "$TMP_DIR"
+echo "Downloading tarball"
+TARBALL="prot-${VERSION}-${OS}-${ARCH}.tar.gz"
+URL="https://github.com/z3co/prot/releases/download/v${VERSION}/${TARBALL}"
+CHECKSUM="https://github.com/z3co/prot/releases/download/v${VERSION}/checksums.txt"
+curl -LO "$URL"
+curl -LO "$CHECKSUM"
+
+echo "Verifying checksums..."
+if command -v sha256sum >/dev/null 2>&1; then
+	sha256sum --check --ignore-missing checksums.txt
+else
+	shasum -a 256 --check --ignore-missing checksums.txt
+fi
+
+echo "Installing binary"
+tar -xzf "$TARBALL"
+sudo mv prot /usr/local/bin/prot
+sudo chmod +x /usr/local/bin/prot
+
+cd /
+rm -rf "$TMP_DIR"
+echo "Done!"
+
+
+
+
